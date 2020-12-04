32092953e7f464bb3a4944f5e4faf274b9aab696
poncem91 <mafe.ponce@gmail.com>
2020-12-03 21:38:54 -0700

    set up RPC worker call to master to request a mapping task

---
+++ mr/rpc.go
@@ -0,0 +1,43 @@
+package mr
+
+//
+// RPC definitions.
+//
+// remember to capitalize all names.
+//
+
+import (
+	"os"
+	"strconv"
+	"time"
+)
+
+const (
+	MAP = "MAP"
+	REDUCE = "REDUCE"
+	IN_PROGRESS = 0
+	COMPLETE = 1
+)
+
+// Add your RPC definitions here.
+type TaskArgs struct {
+}
+
+type Task struct {
+	Filepath string
+	Status int
+	TimeAssigned time.Time
+	Type string
+	NReduce int
+	taskID int
+}
+
+// Cook up a unique-ish UNIX-domain socket name
+// in /var/tmp, for the master.
+// Can't use the current directory since
+// Athena AFS doesn't support UNIX-domain sockets.
+func masterSock() string {
+	s := "/var/tmp/824-mr-"
+	s += strconv.Itoa(os.Getuid())
+	return s
+}
---
+++ mr/worker.go
@@ -0,0 +1,125 @@
+package mr
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+import "log"
+import "net/rpc"
+import "hash/fnv"
+
+
+//
+// Map functions return a slice of KeyValue.
+//
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+//
+// use ihash(key) % NReduce to choose the reduce
+// task number for each KeyValue emitted by Map.
+//
+func ihash(key string) int {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return int(h.Sum32() & 0x7fffffff)
+}
+
+
+//
+// main/mrworker.go calls this function.
+//
+func Worker(mapf func(string, string) []KeyValue,
+	reducef func(string, []string) string) {
+
+	for {
+		assignedTask, err := AskForTask()
+
+		if err != nil {
+			log.Panicln("worker could not retrieve task from master")
+		}
+
+		if assignedTask.Status == COMPLETE {
+			log.Println("tasks have all been completed, quitting")
+			break
+		}
+
+		file, err := os.Open(assignedTask.Filepath)
+		if err != nil {
+			log.Fatalf("cannot open %v", assignedTask.Filepath)
+		}
+		content, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatalf("cannot read %v", assignedTask.Filepath)
+		}
+		file.Close()
+
+		if assignedTask.Type == MAP {
+			intermediateKV := mapf(assignedTask.Filepath, string(content))
+			intermediateFiles := make(map[int]*os.File)
+
+			for _, keyValue := range intermediateKV {
+				reduceTaskNum := ihash(keyValue.Key) % assignedTask.NReduce
+				file, ok := intermediateFiles[reduceTaskNum]
+				if !ok {
+					filename := fmt.Sprintf("mr-%d-%d", assignedTask.taskID, reduceTaskNum)
+					file, err := os.Create(filename)
+					if err != nil {
+						log.Fatal(err)
+					}
+					intermediateFiles[reduceTaskNum] = file
+				}
+				enc := json.NewEncoder(file)
+				err := enc.Encode(&keyValue)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+
+		} else if assignedTask.Type == REDUCE {
+
+		}
+	}
+
+
+
+}
+
+func AskForTask() (*Task, error) {
+	args := TaskArgs{}
+	reply := Task{}
+
+	if call("Master.AssignTask", &args, &reply) {
+		return &reply, nil
+	} else {
+		return nil, errors.New("master couldn't assign task")
+	}
+}
+
+//
+// send an RPC request to the master, wait for the response.
+// usually returns true.
+// returns false if something goes wrong.
+//
+func call(rpcname string, args interface{}, reply interface{}) bool {
+	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	sockname := masterSock()
+	c, err := rpc.DialHTTP("unix", sockname)
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	defer c.Close()
+
+	err = c.Call(rpcname, args, reply)
+	if err == nil {
+		return true
+	}
+
+	fmt.Println(err)
+	return false
+}
